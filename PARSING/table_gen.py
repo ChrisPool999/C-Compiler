c06a42efb17829228ddc94368b053e95a8f607c1
Chris Pool <Brisbool999@gmail.com>
2025-01-21 04:27:43 +0800

    creating table_generator. built find_first() method

--- PARSING/table_gen.py
+++ PARSING/table_gen.py
@@ -1,94 +1,199 @@
+import sys
+from itertools import islice
+from collections import namedtuple
+
+class InputError(Exception):
+    def __init__(self, message: str, input_value: str = None) -> None:
+        super().__init__(message)
+        self.input_value = input_value
+
+    def __str__(self) -> str:
+        if self.input_value is not None:
+            return f"{self.args[0]} (Invalid input: {self.input_value})"
+        
+        return self.args[0]
+
+Rule = namedtuple("Rule", ["LHS", "expansions"])
+
 class Grammer:
-    def __init__(self, filename):
-        self.productionRules = {}
+
+    def __init__(self, filename: str) -> None:
+        self._rules: dict[list:str] = {}
         self._parse_grammer(filename)
-        self.first = {}
-        self.found = {}
 
-    def _parse_grammer(self, filename):
+    def _print_rules(self) -> None:
+        print("\n")
+        for LHS, expansions in self._rules.items():
+            print(LHS)
+            for e in expansions:
+                print("    " + str(e))
+            print("\n")
+
+    def _parse_grammer(self, filename: str) -> None:
         with open(filename, 'r') as file:
+            LHS = ""
             expansions = []
-            symbols = ""
 
-            # fix this, its shit and doesnt even work for the start symbol
-            # it never gets added on line 25
-            old_symbol = ""
-            for line in file:        
-                symbols = line.split()
+            lines:list[str] = file.readlines()
+            for i in range(len(lines)):        
+                symbols:list[str] = lines[i].split()
 
-                if (len(symbols) == 0):
+                if not symbols:
+                    self._rules[LHS] = expansions
+                    expansions = []
                     continue
-
+                
                 if symbols[1] == "::=":
-
-                    if len(expansions):
-                        self.rules()[old_symbol] = expansions
-                        expansions = []
-                        old_symbol = symbols[0]
-
+                    LHS = symbols[0]
                     expansions.append(symbols[2 : len(symbols)])
 
                 elif (symbols[0] == '|'):
                     expansions.append(symbols[1 : len(symbols)])
-            
-            # self.production_rules[symbols[0]] = expansions
+                
+                else:
+                    print(symbols + "\n")
+                    raise InputError("Invalid input", symbols)
 
-    def getRid(self, list):
-        return list[-2] == '}'
+    def _remove_tags(self, symbol: str) -> str:
+        """ Removes punctuation, tags, and extra information from the base symbol \n
+            E.g {<assignment-operator}* --> <assignment-operator"""
 
-    def find_first(self, LHS):
-        if (LHS in self.found):
-            return
-        self.found[LHS] = 123
+        left: int = None
+        right: int = None
 
-        # obviously this is extremely hacky, and was just brute forced
-        # for my amusement 
-        if (LHS[0] == '{'):
-            LHS = LHS[1:len(LHS)]
-        if (len(LHS) >= 2 and LHS[-2] == '}'):
-            LHS = LHS[:-2]
-
-        if not LHS in self.rules():
-            print(LHS)
-            return
+        for i in range(len(symbol)):
+            if symbol[i] == '<':
+                left = i
+            if (symbol[i] == '>'):
+                right = i
         
-        for rule in self.rules()[LHS]:
-            self.find_first(rule[0])
+        if left and right:
+            return symbol[left : right + 1]
+        return symbol
 
-    def rules(self):
-        return self.productionRules
-  
+    def _is_terminal(self, symbol: str) -> bool:
+        return symbol not in self._rules 
 
-# NOTE! ALOT TO FIX, WAS JUST FUCKING AROUND TRYING TO GET IT TO WORK
+    def _is_optional(self, symbol: str) -> bool:
+        return (
+            symbol[-1] in {"?", "*"} or 
+            ("ε" in self._rules.get(symbol, []))
+        )
+
+    def _get_rule(self, index: int) -> Rule:
+        if (index >= len(self._rules.items())):
+            raise IndexError(f"Index {index} is out of bounds")
+        
+        l, r = list(self._rules.items())[index]
+        return Rule(LHS=l, expansions=r)
+
+class TableGenerator:
+    _Item = namedtuple("item", ["rule", "lookahead"])
+
+    def __init__(self, BNF_file, output_file = None):
+        self._grammer = Grammer(BNF_file)
+        self._states: list[list[self.Item]] = [] 
+        self._stack = ["$"]
+        
+        start_rule = self._grammer._get_rule(0)
+        start_item = self._Item(rule=start_rule, lookahead=["$"])
+        self._create_state(start_item)
+
+    def _create_state(self, start_item: _Item) -> None:
+        return 
+        # items = [start_item]
+        # self._closure()
+
+    def _closure(self, item: _Item, state_num: int) -> _Item:
+        # if closure is optional, find closure of next...
+        return
+
+    def _find_follow(self, item: _Item, rule_index: int) -> list[str]:
+        # if next is optional, do find_follow on optional and item after
+        # if next is end of list, use look-ahead 
+        # if current is repetitive, use repetitive, and next  
+        # account for ε...
+        
+        if rule_index == len(item.rule) - 1:
+           return [item.lookahead]
+
+        return
+
+    # handle if the first() symbol is optional in the closure() method
+    def _find_first(self, symbol: str, seen: set = None) -> set[str]:
+        """
+            Finds the terminal symbols that can expand from a symbol\n
+            Will automatically remove any tags from the symbol\n
+            E.g {<specifier-qualifier>}* -> <specifier-qualifier> which is how the LHS is represented in the BNF\n
+
+            Parameters:\n
+            symbol (str): The symbol you want to find the terminals of
+            seen (set): Default Parameter. Avoid passing this argument outside the method. Used to avoid infinite expansions\n
+
+            returns:\n
+            list[str]: strings representing terminal symbols
+        """
+        if seen is None: seen = set()
+        symbol = self._grammer._remove_tags(symbol)
+
+        if symbol in seen: return set()
+        if self._grammer._is_terminal(symbol): return set([symbol])
+
+        seen.add(symbol)
+        terminals = set()
+
+        for RHS in self._grammer._rules[symbol]:
+            for s in RHS:
+                terminals |= self._find_first(s, seen)
+                if not self._grammer._is_optional(s): break
+
+        return terminals
 
 def main():
-    grammer = Grammer("PARSING/BNF.txt")
-    grammer.find_first("<direct-declarator>")
-    print("\n")
-    # grammer.find_first("<declarator>")        
+    table = TableGenerator("PARSING/BNF.txt")
+    print(table._find_first("<declarator>"))
 
 if __name__ == "__main__":
     main()
 
+#____________________________________________________________________________________________________________
+# have to handle repetition, optionals, far right closures (just give previous look-ahead...)
 
-# if word1 = non-terminal & word2 = "::=" THEN RULE
-# if word1 = '|' then its an additional RHS
-# empty line required after each definition ends
+#_____________________________________________________________________________________________________________
+# if its an optional... we want to ALSO do the thing do the symbol after it
+# if its a repetition... we want to ALSO do the thing as IF the next symbol was the closure 
 
-# production rules should be a map of arrays
+# OPTIONAL CLOSURE
+# . {<pointer>}? <direct-declarator>
+# . <direct-declarator>
 
+# REPETITION CLOSURE
+# {<specifier-qualifier>}+ {<abstract-declarator>}?
+# . {<specifier-qualifier>}+ {<abstract-declarator>}?
+# look-ahead for <specifier-qualifier, sends it so the same state, eg . {<specifier-qualifier>}+ {<abstract-declarator>}?
+# look-ahead for {<abstract-declarator>} or follow(LHS) sends it to 2 different states
 
+#_________________________________________________________________________________________________________________________________________
 
-# braces                     {}
-# parenthesis                ()
-# arrays                     []
-# klen star                  *
-# or                         |
-# one or more                +
-# one or none                ?
-# non braces enclosing eg   void
+# closure
+#   - add all rules where the symbol appears on the LHS
+#       - Need to include support for optionals
+#       - Need to include support for repetitions 
+
+# follow()
+#   - get first() possible terminals AFTER the closure symbol
+#       - Need to include support for optionals
+#       - Need to include support for repetitions 
+#       - need to include support for far-right symbols
+# _____________________________________________________________________________________________________
+
+# what happens if a closure is optional?
+#   - perform closure on both the optional and the symbol after, assuming its non-terminal
+
+# what happens if a closure if on the far right?
+#   - the look-ahead will stay the same as the current rule, eg S -> a.X, $  lookahead = $
+
+# what happens if a closure is repetitive?
+#   we must consider both cases if closure symbol appears once or more
 
 # <parameter-list> , ...    -> can optionally append a comma seperated list of parameter-list
-
-# If a punctuation is next to a symbol it has optionality 
-# If a punctuation is seperated by spaces, its a part of the grammer
